1d8f25c0cb3a8fd46f519b50ddb8d60393246efe
Daniel Suarez <suarez.daniel101@gmail.com>
2017-01-18 05:03:03 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
