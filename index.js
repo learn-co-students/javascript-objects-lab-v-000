9f1956e0c85693b4b8a3cc01c463931490cc709e
Bob Habig <rfhab2@gmail.com>
2017-02-14 23:01:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = object;
+  var newObject = Object.assign({}, object);
   delete newObject.key;
   return newObject
 }
