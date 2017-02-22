fc372c4a981d0625f027caf51a6eaddc70dde053
smithwebtek <brad@smithwebtek.com>
2017-02-22 11:36:11 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {} 
+
+var meals = { breakfast: "eggs"}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key]=value;
+  return obj;
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj)
+  newObj; 
+  delete newObj.key
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj.key;
+  return obj;
+}
