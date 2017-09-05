9066d1ddb56e517554161e0d56be7eb1fb69b2b5
mirandalily <mirandaraymond@me.com>
2017-09-05 13:58:11 -0700

    set up functions

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
