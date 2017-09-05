5fbeac4ee8aa86194871bf4eb66eb051acb0e49d
mirandalily <mirandaraymond@me.com>
2017-09-05 14:03:04 -0700

    4 tests passing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-
+  return Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
@@ -13,5 +13,5 @@ function destructivelyDeleteFromObjectByKey(object, key){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+
 }
