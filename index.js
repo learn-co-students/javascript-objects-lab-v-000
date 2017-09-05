321b2278db18847b6858f4aadc617c829d1e0ee7
mirandalily <mirandaraymond@me.com>
2017-09-05 14:05:45 -0700

    7 tests passing

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-
+  delete object[key]
+  return object
 }
 
 function deleteFromObjectByKey(object, key){
